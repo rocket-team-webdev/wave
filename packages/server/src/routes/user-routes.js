85788a8900402dbbdf944259524d550fe0ee2adf
Brahim <brahimcasas@hotmail.es>
2021-09-21 11:09:28 +0200

    feat: firebase implementation backend done

---
+++ packages/server/src/routes/user-routes.js
@@ -0,0 +1,10 @@
+const { userController } = require("../controllers");
+const Router = require("express").Router;
+
+const userRouter = Router();
+
+userRouter.post("/register/", userController.signUp);
+
+module.exports = {
+  userRouter,
+};
--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -3,11 +3,15 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 
+const { userRouter } = require("./routes");
+
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
+
+app.use("api/", userRouter);
 
 module.exports = app;
