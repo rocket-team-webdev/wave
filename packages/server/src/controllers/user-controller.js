a981a6b00e6aa6efb942ad8cea88f4b09778797c
Brahim <brahimcasas@hotmail.es>
2021-09-21 15:21:44 +0200

    feat: create account with firebase

--- packages/server/src/controllers/user-controller.js
+++ packages/server/src/controllers/user-controller.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 
-async function signUp(req, res) {
+async function signUp(req, res, next) {
   try {
     const data = await db.User.create({
       ...req.user,
@@ -10,7 +10,8 @@ async function signUp(req, res) {
       .status(200)
       .send({ message: "Successfully signed up", userId: data._id });
   } catch (error) {
-    res.status(500).send({ error: error.message });
+    res.status(500).send({ error: error });
+    next(error);
   }
 }
 
--- packages/server/src/routes/user-routes.js
+++ packages/server/src/routes/user-routes.js
@@ -1,9 +1,13 @@
 const { userController } = require("../controllers");
 const Router = require("express").Router;
 
+const {
+  authRegisterMiddleware,
+} = require("../middlewares/auth-firebase-middleware");
+
 const userRouter = Router();
 
-userRouter.post("/register", userController.signUp);
+userRouter.post("/register", authRegisterMiddleware, userController.signUp);
 
 module.exports = {
   userRouter,
