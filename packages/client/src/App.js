5879d1f5cf10aa3f034559345a5fc0068a146eb7
Ricard Garcia <imricardgarcia@gmail.com>
2021-09-21 17:25:14 +0200

    User reg auth/form react components/17/main (#36)
    * Feat: Button and input first styles done
    
    * Conditional classes done
    
    * Feat: styles done
    
    * Feat: App test remove
    
    * Feat: Fonts setted
    
    * feat: added colors.scss
    
    * feat: added button component
    
    * feat: added positive and negative inputs
    
    * feat: added file input
    
    * fix: solved bug with input file
    
    * style: removed input in App.js
    
    Co-authored-by: labietelabiete <victorhugogote@gmail.com>

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
 
+import Button from "./components/Button";
+import Input from "./components/Input";
+
 function App() {
-  return <div>Initial Page</div>;
+  return (
+    <div>
+      <Button>Primary button</Button>
+      <Button secondaryBtn>Secondary button</Button>
+      <Button isNegative>Secondary button</Button>
+      <Button isNegative secondaryBtn>
+        Secondary button
+      </Button>
+      <Button isSmall>Secondary button</Button>
+      <Input
+        label="hey"
+        id="aloha"
+        type="password"
+        placeholder="This is a placeholder"
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ packages/client/src/components/Input/Input.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+import "./Input.scss";
+
+export default function FloatInput({
+  label = "input-01",
+  id = "input-01",
+  isNegative = false,
+  type = "",
+  fullWidth = false,
+  placeholder = "",
+  handleChange = () => {},
+  handleBlur = () => {},
+  handleInput = () => {},
+  errorMessage,
+  hasErrorMessage,
+  ...props
+}) {
+  let labelClassNames = "fnt-label-light p-0 ";
+  let inputClassNames = "form-input fnt-input-light fx-rounded ps-3 ";
+  let uploadClassNames =
+    "custom-upload-input fx-rounded fnt-input-light d-flex align-items-center ps-3 w-100 ";
+
+  if (type === "file") {
+    inputClassNames += "upload-input m-0 ";
+    if (!isNegative) {
+      uploadClassNames += "positive-custom-upload-input";
+    } else {
+      uploadClassNames += "negative-custom-upload-input";
+    }
+  }
+
+  if (isNegative) {
+    labelClassNames += "negative-label";
+    inputClassNames += "negative-input";
+  } else {
+    labelClassNames += "positive-label";
+    inputClassNames += "positive-input";
+  }
+
+  if (fullWidth) {
+    inputClassNames += " w-100";
+  }
+
+  return (
+    <div className="custom-input d-flex flex-column mb-1">
+      <label className={labelClassNames} htmlFor={id}>
+        {label}
+      </label>
+      {type === "file" && (
+        <div className={uploadClassNames}>{placeholder} for upload file</div>
+      )}
+      <input
+        type={type}
+        className={inputClassNames}
+        id={id}
+        name={id}
+        placeholder={placeholder}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        onInput={handleInput}
+        {...props}
+      />
+      {hasErrorMessage && errorMessage && (
+        <p className="error-msg">{errorMessage}</p>
+      )}
+    </div>
+  );
+}
