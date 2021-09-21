9b58ba208c40cd7169fe7c3991362c45c82a68e3
labietelabiete <victorhugogote@gmail.com>
2021-09-21 12:37:11 +0200

    Feat: Fonts setted

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import Button from "./components/Button";
+import Input from "./components/Input";
+
 function App() {
-  return <div>First initial page</div>;
+  return (
+    <div>
+      <Button>My button</Button>
+      <Button secondaryBtn fullWidth>
+        My button
+      </Button>
+      <Input />
+      <Input fullWidth />
+    </div>
+  );
 }
 
 export default App;
