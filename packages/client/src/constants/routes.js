cc8609a0de46ddde81732880467cbc78c748104f
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-09-21 10:36:04 +0200

    Created routing and components for signup and home pages

---
+++ packages/client/src/constants/routes.js
@@ -0,0 +1,19 @@
+// PUBLIC PAGES
+
+export const PUBLIC = {
+  HOME: "/",
+  SIGNUP: "/signup",
+  SIGNOUT: "/signout",
+};
+
+// PRIVATE PAGES
+
+export const PRIVATE = {
+  ADMIN_SIGNIN: "/admin",
+};
+
+// API
+
+export const API = {
+  MAIN: "http://localhost:4000",
+};
---
+++ packages/client/src/pages/Public/SignUp/SignUp.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function SignUp() {
+  return (
+    <div>
+      <main className="form-signin">
+        <form>
+          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+          <div className="form-floating">
+            <input
+              type="email"
+              className="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label htmlFor="floatingInput">Email address</label>
+          </div>
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label htmlFor="floatingPassword">Password</label>
+          </div>
+
+          <div className="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me" /> Remember me
+            </label>
+          </div>
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
+            Sign in
+          </button>
+          <p className="mt-5 mb-3 text-muted">© 2017–2021</p>
+        </form>
+      </main>
+    </div>
+  );
+}
