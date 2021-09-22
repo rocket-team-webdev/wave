57c6d2e4ee5b9d416536cf940caf56ddc879f1df
labietelabiete <victorhugogote@gmail.com>
2021-09-22 17:47:04 +0200

    Feat: User account showing and editing done Closes User info visualization #12  Closes User info edition #13

--- packages/client/src/pages/Private/Account/Account.js
+++ packages/client/src/pages/Private/Account/Account.js
@@ -45,7 +45,6 @@ export default function Account() {
   async function loadAccount() {
     try {
       const { data } = await getAccount();
-      console.log(data);
       formik.setValues({
         username: data.data.username || "",
         gender: "Male" || "",
