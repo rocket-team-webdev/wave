780d14ba5de93eb014152e697ec580a3940f536b
labietelabiete <victorhugogote@gmail.com>
2021-09-22 11:41:18 +0200

    Feat: Update done

--- packages/client/src/api/account-api.js
+++ packages/client/src/api/account-api.js
@@ -26,6 +26,14 @@ export async function getAccount(api = makeAccountApi()) {
   });
 }
 
+export async function updateAccount(data, api = makeAccountApi()) {
+  const token = await getCurrentUserToken();
+  return api.post(``, {
+    headers: { Authorization: `Bearer ${token}` },
+    data: data,
+  });
+}
+
 export async function createClient(clientData, api = makeRegisterApi()) {
   const token = await getCurrentUserToken();
   return api.post(
--- packages/client/src/pages/Private/Account/Account.js
+++ packages/client/src/pages/Private/Account/Account.js
@@ -4,8 +4,9 @@ import { useFormik } from "formik";
 import updateSchema from "./update-schema";
 
 import Input from "../../../components/Input";
+import Button from "../../../components/Button";
 
-import { getAccount } from "../../../api/account-api";
+import { getAccount, updateAccount } from "../../../api/account-api";
 
 export default function Account() {
   const [accountData, setAccountData] = useState([]);
@@ -18,6 +19,7 @@ export default function Account() {
     try {
       const { data } = await getAccount();
       setAccountData(data, accountData);
+      console.log("accountData", accountData);
       console.log(data);
       // setLoadStatus({ isError: false, isLoading: false });
     } catch (error) {
@@ -27,12 +29,13 @@ export default function Account() {
 
   useEffect(() => {
     loadAccount();
+    console.log("Setting initial values");
   }, []);
 
   const formik = useFormik({
     initialValues: {
       username: accountData.username,
-      // gender: accountData.gender,
+      gender: "",
       profilePicture: accountData.profilePicture,
       firstName: accountData.firstName,
       lastName: accountData.lastName,
@@ -41,125 +44,129 @@ export default function Account() {
       country: accountData.country,
     },
     validationSchema: updateSchema,
-    onSubmit: async () => {
+    onSubmit: async (values) => {
+      console.log(values);
       // eslint-disable-next-line
-      //Update function
+      const { data } = await updateAccount();
     },
   });
 
   return (
-    <div className="container">
+    <div className="mx-5">
       <div className="row">
         <div className="col-5">
-          <h1>Username</h1>
-          <p>ACOUNT DETAILS</p>
-          <p>PASSWORD RECOVERY</p>
-          <p>PASSWORD UPDATE</p>
-          <p>LOGOUT</p>
+          <h1 className="fnt-jumbo">Username</h1>
+          <p className="fnt-subtitle-bold mb-0 lh-1">ACOUNT DETAILS</p>
+          <p className="fnt-subtitle-light mb-0 lh-1">PASSWORD RECOVERY</p>
+          <p className="fnt-subtitle-light mb-0 lh-1">PASSWORD UPDATE</p>
+          <p className="fnt-subtitle-light mb-0 lh-1">LOGOUT</p>
         </div>
         <div className="col-7">
-          <h1>Account details</h1>
-          <form onSubmit={formik.handleSubmit}>
-            <div className="d-flex">
-              <Input
-                type="text"
-                label="USERNAME"
-                id="username"
-                value={formik.values.username}
-                errorMessage={formik.errors.username}
-                hasErrorMessage={formik.touched.username}
-                placeholder={accountData.username}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
-              <select
-                label="GENDER"
-                className="gender-select"
-                value={formik.values.gender}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                id="gender"
-              >
-                <option value="Spain">Male</option>
-                <option value="Italy">Female</option>
-              </select>
+          <h1 className="fnt-page-title mb-4">Account details</h1>
+          <form onSubmit={formik.handleSubmit} className="row">
+            <Input
+              classNames="col-4"
+              type="text"
+              label="USERNAME"
+              id="username"
+              value={formik.values.username}
+              errorMessage={formik.errors.username}
+              hasErrorMessage={formik.touched.username}
+              placeholder={accountData.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            <select
+              label="GENDER"
+              className="gender-select col-4"
+              value={formik.values.gender}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              id="gender"
+            >
+              <option value="Spain">Male</option>
+              <option value="Italy">Female</option>
+            </select>
 
-              <Input
-                type="file"
-                label="PROFILE IMAGE"
-                id="profileImage"
-                value={formik.values.profileImage}
-                errorMessage={formik.errors.profileImage}
-                hasErrorMessage={formik.touched.profileImage}
-                placeholder={accountData.profileImage}
-                onChange={formik.profileImage}
-                onBlur={formik.profileImage}
-              />
-            </div>
-            <div className="d-flex">
-              <Input
-                type="text"
-                label="FIRST NAME"
-                id="firstName"
-                value={formik.values.firstName}
-                errorMessage={formik.errors.firstName}
-                hasErrorMessage={formik.touched.firstName}
-                placeholder={accountData.firstName}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
-              <Input
-                type="text"
-                label="LAST NAME"
-                id="lastName"
-                value={formik.values.lastName}
-                errorMessage={formik.errors.lastName}
-                hasErrorMessage={formik.touched.lastName}
-                placeholder={accountData.lastName}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
-              <Input
-                type="date"
-                label="BIRTHDATE"
-                id="birthdate"
-                value={formik.values.birthdate}
-                errorMessage={formik.errors.birthdate}
-                hasErrorMessage={formik.touched.birthdate}
-                placeholder={accountData.birthDate}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
-            </div>
-            <div className="d-flex">
-              <Input
-                type="email"
-                label="EMAIL"
-                id="email"
-                value={formik.values.email}
-                errorMessage={formik.errors.email}
-                hasErrorMessage={formik.touched.email}
-                placeholder={accountData.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
-              <select
-                className="country-select"
-                value={formik.values.country}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                id="country"
-              >
-                <option value="Spain">Spain</option>
-                <option value="Argentina">Argentina</option>
-                <option value="Morocco">Morocco</option>
-                <option value="France">France</option>
-                <option value="Italy">Italy</option>
-                <option value="Germany">Germany</option>
-                <option value="USA">USA</option>
-                <option value="Mexico">Mexico</option>
-                <option value="Catalonia">Catalonia</option>
-              </select>
+            <Input
+              classNames="col-4"
+              type="file"
+              label="PROFILE IMAGE"
+              id="profileImage"
+              value={formik.values.profileImage}
+              errorMessage={formik.errors.profileImage}
+              hasErrorMessage={formik.touched.profileImage}
+              placeholder={accountData.profileImage}
+              onChange={formik.profileImage}
+              onBlur={formik.profileImage}
+            />
+            <Input
+              classNames="col-4"
+              type="text"
+              label="FIRST NAME"
+              id="firstName"
+              value={formik.values.firstName}
+              errorMessage={formik.errors.firstName}
+              hasErrorMessage={formik.touched.firstName}
+              placeholder={accountData.firstName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            <Input
+              classNames="col-4"
+              type="text"
+              label="LAST NAME"
+              id="lastName"
+              value={formik.values.lastName}
+              errorMessage={formik.errors.lastName}
+              hasErrorMessage={formik.touched.lastName}
+              placeholder={accountData.lastName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            <Input
+              classNames="col-4"
+              type="date"
+              label="BIRTHDATE"
+              id="birthdate"
+              value={formik.values.birthdate}
+              errorMessage={formik.errors.birthdate}
+              hasErrorMessage={formik.touched.birthdate}
+              placeholder={accountData.birthDate}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            <Input
+              classNames="col-8"
+              type="email"
+              label="EMAIL"
+              id="email"
+              value={formik.values.email}
+              errorMessage={formik.errors.email}
+              hasErrorMessage={formik.touched.email}
+              placeholder={accountData.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            <select
+              className="country-select col-4"
+              value={formik.values.country}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              id="country"
+            >
+              <option value="Spain">Spain</option>
+              <option value="Argentina">Argentina</option>
+              <option value="Morocco">Morocco</option>
+              <option value="France">France</option>
+              <option value="Italy">Italy</option>
+              <option value="Germany">Germany</option>
+              <option value="USA">USA</option>
+              <option value="Mexico">Mexico</option>
+              <option value="Catalonia">Catalonia</option>
+            </select>
+            <div className="col-12 text-end mt-5">
+              <Button>Edit</Button>
             </div>
           </form>
         </div>
