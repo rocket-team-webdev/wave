6e0a4da4ef696e233987ea28326e6540bd629772
Ricard Garcia <imricardgarcia@gmail.com>
2021-09-27 20:00:26 +0200

    feat: added initial styles to home elements & added searchbar

--- packages/client/src/components/PlaylistCard/PlaylistCard.jsx
+++ packages/client/src/components/PlaylistCard/PlaylistCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function PlaylistCard({ classNames, playlistName }) {
-  const componentClasses = `${classNames} d-flex align-items-end playlist-card clr-light-20 fx-rounded`;
+  const componentClasses = `${classNames} d-flex align-items-end playlist-card fx-rounded`;
   return (
     <div className={componentClasses}>
       <p className="fnt-input-bold fnt-light ps-3">
--- packages/client/src/pages/Public/Home/Home.jsx
+++ packages/client/src/pages/Public/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import Layout from "../../../components/Layout";
 import JumboText from "../../../components/JumboText";
@@ -6,8 +7,12 @@ import HomeElement from "../../../components/HomeElement";
 import Button from "../../../components/Button";
 import PlaylistCard from "../../../components/PlaylistCard";
 import ArtistCard from "../../../components/ArtistCard";
+import Input from "../../../components/Input";
 
 export default function Home() {
+  const userState = useSelector((state) => state.user);
+  const userFirstName = userState.firstName;
+
   const genresList = [
     "soul",
     "funk",
@@ -39,9 +44,19 @@ export default function Home() {
 
   return (
     <Layout>
-      <div className="d-flex justify-content-between row p-0 g-4">
-        <JumboText secText="Username" cols="6" isNegative />
-        <div className="col col-12 col-md-5 clr-light-20 fx-rounded p-0">
+      <div className="d-flex justify-content-between align-items-start row p-0 g-4">
+        <div className="col col-12 col-md-6 p-0 left-side">
+          <JumboText secText={userFirstName} cols="12" isNegative />
+          <form className="mt-5" action="">
+            <Input
+              label="searchbar"
+              id="searchbar"
+              classNames="col col-12 col-md-6"
+              isNegative
+            />
+          </form>
+        </div>
+        <div className="col col-12 col-md-5 fx-rounded p-0">
           {/* Genres */}
           <HomeElement label="Genres">
             {/* !!! TODO map genres from user */}
