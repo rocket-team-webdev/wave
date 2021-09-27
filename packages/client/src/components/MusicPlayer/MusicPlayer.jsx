c27860e5b1089d6a212a0834c84f96d6be609a0f
labietelabiete <victorhugogote@gmail.com>
2021-09-27 17:58:09 +0200

    Feat: First styles approaching

--- packages/client/src/components/MusicPlayer/MusicPlayer.jsx
+++ packages/client/src/components/MusicPlayer/MusicPlayer.jsx
@@ -3,12 +3,24 @@ import React from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 
+import { FaPlay, FaPause } from "react-icons/fa";
+// import { IoMdRepeat } from "react-icons/io";
+
+import "./MusicPlayer.scss";
+
 export default function MusicPlayer() {
   return (
     <div>
       <AudioPlayer
-        autoPlay
+        // autoPlay
+        showSkipControls
+        showJumpControls={false}
         src="https://res.cloudinary.com/dz5nspe7f/video/upload/v1632147267/music-uploads/bensound-creativeminds_vjqm2b.mp3"
+        layout="horizontal-reverse"
+        customIcons={{
+          play: <FaPlay />,
+          pause: <FaPause style={{ color: "red" }} />,
+        }}
         // onPlay={(e) => console.log("onPlay")}
         // other props here
       />
