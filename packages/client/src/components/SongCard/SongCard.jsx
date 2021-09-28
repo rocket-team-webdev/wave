1bff1d5e177c4610b3eda4815f79f01e7ec6db39
Nachomontoya <montoyanacho@gmail.com>
2021-09-28 18:25:23 +0200

    feat: added SongCard component

--- packages/client/src/components/SongCard/SongCard.jsx
+++ packages/client/src/components/SongCard/SongCard.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function SongCard() {
-  return <div className="col col-12 border">Hello</div>;
+export default function SongCard({
+  songNumber = 1,
+  songImg,
+  songName = "Glory Box",
+  artist = "Portishead",
+  albumName = "Dummy",
+  time = "3:45",
+}) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    setIsLiked(!isLiked);
+  };
+
+  return (
+    <div className="col col-12 border d-flex align-items-center justify-content-between">
+      <p className="m-0">{songNumber}</p>
+      <div className="clr-light border">
+        <img src={songImg} alt={songName} />
+      </div>
+      <div className="heart-wrapper d-flex justify-content-end fnt-primary">
+        <button className="heart-button" type="button" onClick={handleLike}>
+          {isLiked ? (
+            <i className="fas fa-heart fnt-secondary" />
+          ) : (
+            <i className="far fa-heart fnt-secondary" />
+          )}
+        </button>
+      </div>
+      <h5>{songName}</h5>
+      <h6>{artist}</h6>
+      <h6>{albumName}</h6>
+      <h6>{time}</h6>
+      <div>···</div>
+    </div>
+  );
 }
--- packages/client/src/pages/Public/Tracks/Tracks.jsx
+++ packages/client/src/pages/Public/Tracks/Tracks.jsx
@@ -5,6 +5,41 @@ import Button from "../../../components/Button";
 import JumboText from "../../../components/JumboText";
 import SongCard from "../../../components/SongCard/SongCard";
 
+const songs = [
+  {
+    songNumber: 1,
+    songImg: "",
+    songName: "Glory Box",
+    artist: "Portishead",
+    albumName: "Dummy",
+    time: "3:45",
+  },
+  {
+    songNumber: 2,
+    songImg: "",
+    songName: "Song 2",
+    artist: "Portishead",
+    albumName: "Dummy",
+    time: "3:45",
+  },
+  {
+    songNumber: 3,
+    songImg: "",
+    songName: "Song 3",
+    artist: "Portishead",
+    albumName: "Dummy",
+    time: "3:45",
+  },
+  {
+    songNumber: 4,
+    songImg: "",
+    songName: "Song 4",
+    artist: "Portishead",
+    albumName: "Dummy",
+    time: "3:45",
+  },
+];
+
 export default function Songs() {
   return (
     <Layout isNegative>
@@ -18,16 +53,20 @@ export default function Songs() {
       <div className="row">
         <div className="col-6">
           <h1>Uploaded</h1>
-          <SongCard />
-          {/* {playlistsList.map((songName) => (
-            <SongCard key={playlistName} songName={songName} hasHeart />
-          ))} */}
+          {songs &&
+            songs.map((song) => (
+              <SongCard
+                key={song._id}
+                songNumber={song.songNumber}
+                songImg={song.songImg}
+                groupName={song.groupName}
+                artist={song.albumName}
+                time={song.time}
+              />
+            ))}
         </div>
         <div className="col-6">
           <h1>Liked</h1>
-          {/* {playlistsList.map((songName) => (
-            <SongCard key={playlistName} songName={songName} hasHeart />
-          ))} */}
         </div>
       </div>
     </Layout>
