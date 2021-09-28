d142d8ea136de8c19368c866e5c23473dffe73ff
Ricard Garcia <imricardgarcia@gmail.com>
2021-09-28 15:15:03 +0200

    feat: added heart to playlists

--- packages/client/src/components/PlaylistCard/PlaylistCard.jsx
+++ packages/client/src/components/PlaylistCard/PlaylistCard.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function PlaylistCard({ classNames, playlistName }) {
-  const componentClasses = `${classNames}col col-12 col-md-6 d-flex align-items-end playlist-card fx-rounded`;
+export default function PlaylistCard({
+  classNames,
+  playlistName,
+  hasHeart = false,
+}) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    setIsLiked(!isLiked);
+  };
+
+  const componentClasses = `${classNames}col col-12 col-md-6 d-flex flex-column justify-content-between playlist-card fx-rounded p-4`;
   return (
     <div className={componentClasses}>
-      <p className="fnt-input-bold fnt-light ps-3">
+      <div className="heart-wrapper d-flex justify-content-end fnt-primary">
+        {hasHeart ? (
+          <button className="heart-button" type="button" onClick={handleLike}>
+            {isLiked ? (
+              <i className="fas fa-heart fnt-secondary" />
+            ) : (
+              <i className="far fa-heart fnt-secondary" />
+            )}
+          </button>
+        ) : (
+          <p>&nbsp;</p>
+        )}
+      </div>
+
+      <p className="fnt-input-bold fnt-light mb-0 truncate">
         {playlistName.toUpperCase()}
       </p>
     </div>
--- packages/client/src/pages/Public/Home/Home.jsx
+++ packages/client/src/pages/Public/Home/Home.jsx
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 import Layout from "../../../components/Layout";
+import HomePopular from "../../../components/HomePopular";
+import HomeMyWave from "../../../components/HomeMyWave";
 import JumboText from "../../../components/JumboText";
-import HomeElement from "../../../components/HomeElement";
-import Button from "../../../components/Button";
-import PlaylistCard from "../../../components/PlaylistCard";
-import ArtistCard from "../../../components/ArtistCard";
 import Input from "../../../components/Input";
 import RadioButtons from "../../../components/RadioButtons";
 
@@ -47,13 +45,11 @@ export default function Home() {
   ];
 
   const handleChangeView = (e) => {
-    console.log(e.target.id);
     if (e.target.id === "myWaveRadio") {
       setpopularView(false);
     } else {
       setpopularView(true);
     }
-    console.log(popularView);
   };
 
   return (
@@ -78,38 +74,16 @@ export default function Home() {
           </div>
           {/* Switch view */}
           {popularView ? (
-            <div>
-              {/* Genres */}
-              <HomeElement label="Genres">
-                {genresList.map((genre) => (
-                  <div key={genre} className="mb-2 me-2">
-                    <Button isSmall>{genre.toUpperCase()}</Button>
-                  </div>
-                ))}
-              </HomeElement>
-              {/* Artists */}
-              <HomeElement label="Artists">
-                {artistsList.map((artistName) => (
-                  <ArtistCard
-                    classNames="me-4 mb-3"
-                    key={artistName}
-                    artistName={artistName}
-                  />
-                ))}
-              </HomeElement>
-              {/* Playlists */}
-              <HomeElement label="Playlists">
-                {playlistsList.map((playlistName) => (
-                  <PlaylistCard
-                    key={playlistName}
-                    classNames="mb-3"
-                    playlistName={playlistName}
-                  />
-                ))}
-              </HomeElement>
-            </div>
+            <HomePopular
+              genresList={genresList}
+              artistsList={artistsList}
+              playlistsList={playlistsList}
+            />
           ) : (
-            <p>User view</p>
+            <HomeMyWave
+              artistsList={artistsList}
+              playlistsList={playlistsList}
+            />
           )}
         </div>
       </div>
