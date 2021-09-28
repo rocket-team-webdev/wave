e7b5dd7d4ddcdf0be3a20637052d4d7cea379f85
labietelabiete <victorhugogote@gmail.com>
2021-09-28 13:32:34 +0200

    Feat: Song information first approaching

--- packages/client/src/components/MusicPlayer/MusicPlayer.jsx
+++ packages/client/src/components/MusicPlayer/MusicPlayer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import AudioPlayer, { RHAP_UI } from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 
-import { FaPlay, FaPause } from "react-icons/fa";
+import { FaPlay, FaPause, FaRegHeart, FaHeart } from "react-icons/fa";
 import { IoMdRepeat } from "react-icons/io";
 import { ImShuffle } from "react-icons/im";
 
@@ -11,8 +11,32 @@ import "./MusicPlayer.scss";
 
 export default function MusicPlayer() {
   const isShuffle = true;
+  const isLiked = false;
   return (
-    <div>
+    <div className="d-flex audio-player">
+      <div className="song-info-wrapper d-flex align-items-center justify-content-between clr-white ps-5 pe-4">
+        <div className="thumb-album">
+          <img
+            src="https://loudcave.es/wp-content/uploads/2021/04/cbf7b52dab62a3eb745e6730068abc4a.1000x1000x1.jpg"
+            alt="album-cover"
+            className="thumb-album-img"
+          />
+        </div>
+        <button type="button" className="like-button">
+          {" "}
+          {isLiked ? (
+            <FaHeart className="like-icon" />
+          ) : (
+            <FaRegHeart className="like-icon" />
+          )}
+        </button>
+        <div className="song-text d-flex">
+          <p className="song-tittle mb-0 fnt-song-bold lh-1 pe-4">
+            Like Toy Soldiers
+          </p>
+          <p className="artist-tittle mb-0 fnt-song-light lh-1">Eminem</p>
+        </div>
+      </div>
       <AudioPlayer
         // autoPlay
         showSkipControls
@@ -41,6 +65,7 @@ export default function MusicPlayer() {
           </div>,
           RHAP_UI.VOLUME_CONTROLS,
         ]}
+        className="pe-5"
         // onPlay={(e) => console.log("onPlay")}
         // other props here
       />
