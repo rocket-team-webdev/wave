56b13bc00638c88a1eda2bac3074cae3088a9b90
Brahim <brahimcasas@hotmail.es>
2021-09-28 10:28:40 +0200

    feat: upload page router

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -17,6 +17,7 @@ import SignIn from "./pages/Public/SignIn";
 import Account from "./pages/Public/Account/Account";
 import Reauthenticate from "./pages/Public/Reauthenticate";
 import ResetPassword from "./pages/Public/ResetPassword";
+import Upload from "./pages/Public/Upload";
 
 function App() {
   const dispatch = useDispatch();
@@ -82,6 +83,9 @@ function App() {
             <OnlyPublicRoute path={PUBLIC.SIGN_IN}>
               <SignIn />
             </OnlyPublicRoute>
+            <PrivateRoute path={PUBLIC.TRACK_UPLOAD}>
+              <Upload />
+            </PrivateRoute>
             <PrivateRoute path={PUBLIC.HOME}>
               <Home />
             </PrivateRoute>
