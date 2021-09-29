53938fb5e985b46113fc27799d04633c243aef2b
Ricard Garcia <imricardgarcia@gmail.com>
2021-09-29 13:02:46 +0200

    feat: added user card

--- packages/client/src/components/UserCard/UserCard.jsx
+++ packages/client/src/components/UserCard/UserCard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import "./UserCard.scss";
+
 export default function UserCard({ userName }) {
   return (
-    <div className="d-flex align-items-center fnt-light me-4">
+    <div className="d-flex align-items-center fnt-light me-4 mb-2 user-card">
       <i className="fas fa-user-circle me-2 fs-4" />
-      <p className="mb-0 fnt-caption">{userName}</p>
+      <p className="mb-0 fnt-caption truncate">{userName}</p>
     </div>
   );
 }
