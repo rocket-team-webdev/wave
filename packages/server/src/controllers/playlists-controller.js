de679c73fe5c81a3dd9304bf81ce2548e8e48ae2
Ernest-DT <ernest.duocastella@gmail.com>
2021-10-04 18:11:14 +0200

    feat: getPlaylist WIP

--- packages/server/src/controllers/playlists-controller.js
+++ packages/server/src/controllers/playlists-controller.js
@@ -15,6 +15,40 @@ async function getPlaylists(req, res, next) {
   }
 }
 
+async function getPlaylist(req, res, next) {
+  try {
+    const { id } = req.params;
+    const { page = 0, limit = 5 } = req.query;
+
+    const playlist = await db.Playlist.findOne(
+      { _id: id },
+      {
+        name: 1,
+        artist: 1,
+        // likes: { $size: "$likedBy" },
+        // isLiked: { $setIsSubset: [[userId], "$likedBy"] },
+        popularity: 1,
+        color: 1,
+        // released: 1,
+        genreId: 1,
+        userId: 1,
+        album: 1,
+        duration: 1,
+        url: 1,
+      },
+    )
+      .populate("tracks")
+      .skip(parseInt(page) * parseInt(limit))
+      .limit(parseInt(limit));
+
+    res.status(200).send({ data: playlist.tracks });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+    next(err);
+  }
+}
+
 module.exports = {
-  getPlaylists: getPlaylists,
+  getPlaylists,
+  getPlaylist,
 };
--- packages/server/src/routes/playlists-routes.js
+++ packages/server/src/routes/playlists-routes.js
@@ -1,9 +1,23 @@
 const { playlistsController } = require("../controllers");
 const Router = require("express").Router;
 
+const {
+  authFirebaseMiddleware,
+} = require("../middlewares/auth-firebase-middleware");
+
 const playlistsRouter = Router();
 
-playlistsRouter.get("/", playlistsController.getPlaylists);
+playlistsRouter.get(
+  "/",
+  authFirebaseMiddleware,
+  playlistsController.getPlaylists,
+);
+
+playlistsRouter.get(
+  "/:id",
+  authFirebaseMiddleware,
+  playlistsController.getPlaylist,
+);
 
 module.exports = {
   playlistsRouter,
