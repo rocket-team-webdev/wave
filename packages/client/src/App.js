28938152dbbc51ce936f2877ebbd1273823d2e8f
Ernest-DT <ernest.duocastella@gmail.com>
2021-10-04 16:43:44 +0200

    feat: playlist song page

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -23,6 +23,7 @@ import TrackUpload from "./pages/Public/TrackUpload";
 import Tracks from "./pages/Public/Tracks";
 import MusicPlayer from "./components/MusicPlayer";
 import CreateAlbum from "./pages/Public/CreateAlbum";
+import PlaylistSongs from "./pages/Public/PlaylistSongs/PlaylistSongs";
 
 function App() {
   const dispatch = useDispatch();
@@ -99,6 +100,9 @@ function App() {
             </PrivateRoute>
             <PrivateRoute path={PUBLIC.MY_SONGS}>
               <Tracks />
+            </PrivateRoute>
+            <PrivateRoute path={PUBLIC.PLAYLIST_SONGS}>
+              <PlaylistSongs />
             </PrivateRoute>
             <PrivateRoute path={PUBLIC.HOME}>
               <Home />
--- packages/client/src/components/JumboText/JumboText.jsx
+++ packages/client/src/components/JumboText/JumboText.jsx
@@ -10,10 +10,16 @@ export default function JumboText({
     <div
       className={`jumbo-text fnt-jumbo mt-0 pt-0 fnt-jumbo fnt-uppercase col col-12 col-md-${cols}`}
     >
-      <p className={isNegative ? "fnt-light mb-0" : "fnt-primary mb-0"}>
+      <p
+        className={
+          isNegative
+            ? "fnt-light mb-0 text-break"
+            : "fnt-primary mb-0 text-break"
+        }
+      >
         {priText}
       </p>
-      {secText && <p className="fnt-secondary mb-0">{secText}</p>}
+      {secText && <p className="fnt-secondary mb-0 text-break">{secText}</p>}
     </div>
   );
 }
