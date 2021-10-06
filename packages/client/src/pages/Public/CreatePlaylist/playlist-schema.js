0fdbcf125e0b1df5478b019792f8c71bfa75ab19
Brahim <brahimcasas@hotmail.es>
2021-10-06 09:16:54 +0200

    feat: create playlist schema

--- packages/client/src/pages/Public/CreatePlaylist/playlist-schema.js
+++ packages/client/src/pages/Public/CreatePlaylist/playlist-schema.js
@@ -1,8 +1,7 @@
 import * as Yup from "yup";
 
 const playlistSchema = Yup.object().shape({
-  title: Yup.string().required("The title is required"),
-  year: Yup.number().required("The release year is required"),
+  name: Yup.string().required("Playlist name is required"),
 });
 
 export default playlistSchema;
