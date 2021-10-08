d6bcaea147687fc15547569699842877a1dd29db
Brahim <brahimcasas@hotmail.es>
2021-10-08 09:21:18 +0200

    test: add provider to wrap pages

---
+++ packages/client/src/utils/test-utils.js
@@ -0,0 +1,31 @@
+// test-utils.jsx
+import React from "react";
+import { render as rtlRender } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+// Import your own reducer
+import userReducer from "../redux/user/reducer";
+import queueReducer from "../redux/music-queue/reducer";
+
+function render(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({
+      reducer: { user: userReducer, queue: queueReducer },
+      preloadedState,
+    }),
+    ...renderOptions
+  } = {},
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+// re-export everything
+export * from "@testing-library/react";
+// override render method
+export { render };
