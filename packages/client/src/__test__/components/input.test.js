5f5b8c181c5beb49a7897ea0f67674c867ccfdd7
Ernest-DT <ernest.duocastella@gmail.com>
2021-10-11 12:55:24 +0200

    test: input component finished

--- packages/client/src/__test__/components/input.test.js
+++ packages/client/src/__test__/components/input.test.js
@@ -1,13 +1,32 @@
 import React from "react";
 
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
 import Input from "../../components/Input";
 
-describe("Input component tests", () => {
-  test("Check input label", () => {
-    render(<Input label="waveApp" />);
-    expect(screen.getByLabelText(/waveApp/)).toBeInTheDocument();
+describe("Input Component", () => {
+  it("renders", () => {
+    render(<Input label="renderedInput" />);
+    expect(screen.getByLabelText(/renderedinput/i)).toBeInTheDocument();
+  });
+  it("uploads a file and shows the name", () => {
+    render(<Input type="file" label="fileInput" />);
+
+    const inputEl = screen.getByLabelText(/fileInput/);
+
+    const file = new File([""], "testImg.png", {
+      type: "image/png",
+    });
+
+    userEvent.upload(inputEl, file);
+
+    expect(screen.getByText(/testImg\.png/)).toBeInTheDocument();
+  });
+  it("when gets an error shows the message", () => {
+    render(<Input hasErrorMessage errorMessage="testError" />);
+
+    expect(screen.getByText(/testError/)).toBeInTheDocument();
   });
 });
