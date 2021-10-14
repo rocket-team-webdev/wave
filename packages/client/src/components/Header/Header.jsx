0964970bcd559974ac6f5ade9ffed71fc9c8cb2f
Ricard Garcia <imricardgarcia@gmail.com>
2021-10-14 18:03:25 +0200

    feat: styled navlinks

--- packages/client/src/components/Header/Header.jsx
+++ packages/client/src/components/Header/Header.jsx
@@ -1,28 +1,33 @@
 import React from "react";
-import { useSelector /* useDispatch */ } from "react-redux";
-import { Link, NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, NavLink, useHistory } from "react-router-dom";
 import { PUBLIC } from "../../constants/routes";
-// import { logOut } from "../../redux/user/actions";
-// import { signOut } from "../../services/auth";
+import { logOut } from "../../redux/user/actions";
+import { signOut } from "../../services/auth";
 
 import waveappLogo from "../../assets/images/waveapp-logotype.svg";
 
 // import Button from "../Button";
 
 import "./Header.scss";
-// import { clearQueue } from "../../redux/music-queue/actions";
+import { clearQueue } from "../../redux/music-queue/actions";
 
 function Header({ props }) {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const userState = useSelector((state) => state.user);
 
-  const navlinkClasses = "fnt-caption d-flex align-items-center fnt-white";
+  const history = useHistory();
 
-  // const handleSignOut = async () => {
-  //   await signOut();
-  //   dispatch(logOut());
-  //   dispatch(clearQueue());
-  // };
+  const navlinkClasses =
+    "fnt-caption d-flex justify-content-center align-items-center fnt-white nav-link";
+
+  const handleSignOut = async () => {
+    await signOut();
+    dispatch(logOut());
+    dispatch(clearQueue());
+    history.push(PUBLIC.SIGN_IN);
+  };
+
   return (
     <header
       {...props}
@@ -36,53 +41,81 @@ function Header({ props }) {
             </Link>
           </div>
         </div>
-        <div className="col-5 d-flex justify-content-end align-items-center p-0">
-          <div className="d-flex justify-content-between">
-            {/* {userState.isLogged && (
-              <div className="">
-                <Link to={PUBLIC.SIGN_IN}>
-                  <Button isSmall handleClick={handleSignOut}>
-                    Sign out
-                  </Button>
-                </Link>
-              </div>
-            )} */}
-            <div className="main-navigation d-flex">
-              <NavLink className={navlinkClasses} to={PUBLIC.MY_SONGS}>
-                My songs
-              </NavLink>
-              <NavLink className={navlinkClasses} to={PUBLIC.MY_PLAYLISTS}>
-                My playlists
-              </NavLink>
-              <NavLink className={navlinkClasses} to="/">
-                My albums
-              </NavLink>
-            </div>
-            <NavLink
-              to={`${PUBLIC.USER_VIEW}/${userState.mongoId}`}
-              className={navlinkClasses}
-            >
-              <div>{userState.firstName}</div>
-            </NavLink>
-
-            {userState.isLogged && (
-              <div>
-                <button
-                  type="button"
-                  id="contextUserMenu"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
+        {userState.isLogged && (
+          <div className="col-5 d-flex justify-content-end align-items-center p-0">
+            <div className="d-flex justify-content-between">
+              <div className="main-navigation d-none d-md-flex">
+                <NavLink
+                  className={navlinkClasses}
+                  activeClassName="active-navlink"
+                  to={PUBLIC.MY_SONGS}
                 >
-                  <img
-                    className="profile-picture fx-rounded"
-                    src={userState.profilePicture}
-                    alt="profilePicture"
-                  />
-                </button>
+                  My songs
+                </NavLink>
+                <NavLink
+                  className={navlinkClasses}
+                  activeClassName="active-navlink"
+                  to={PUBLIC.MY_PLAYLISTS}
+                >
+                  My playlists
+                </NavLink>
+                <NavLink
+                  className={navlinkClasses}
+                  activeClassName="active-navlink"
+                  to="/"
+                >
+                  My albums
+                </NavLink>
               </div>
-            )}
+              <NavLink
+                to={`${PUBLIC.USER_VIEW}/${userState.mongoId}`}
+                className="fnt-caption d-flex justify-content-center align-items-center fnt-white user-link truncate"
+              >
+                <div>{userState.firstName}</div>
+              </NavLink>
+
+              {userState.isLogged && (
+                <div>
+                  <button
+                    type="button"
+                    id="contextUserMenu"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <img
+                      className="profile-picture fx-rounded"
+                      src={userState.profilePicture}
+                      alt="profilePicture"
+                    />
+                  </button>
+                  <ul
+                    className="dropdown-menu dropdown-menu-end clr-secondary p-1"
+                    aria-labelledby="contextUserMenu"
+                  >
+                    <li>
+                      <button
+                        className="dropdown-item fnt-light fnt-song-regular "
+                        type="button"
+                        onClick={() => {}}
+                      >
+                        User account
+                      </button>
+                    </li>
+                    <li>
+                      <button
+                        className="dropdown-item fnt-light fnt-song-regular "
+                        type="button"
+                        onClick={handleSignOut}
+                      >
+                        Sign out
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </header>
   );
