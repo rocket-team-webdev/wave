291ea7c5664a7a38582b78cba123209071d93e7e
Brahim <brahimcasas@hotmail.es>
2021-10-16 17:07:23 +0200

    feat: render top reproduced songs

--- packages/client/src/components/Chart/Chart.jsx
+++ packages/client/src/components/Chart/Chart.jsx
@@ -1,35 +1,39 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 
-const Chart = ({ chartData }) => {
-  const cgf = {
-    labels: ["first", "second"],
-    datasets: [
-      {
-        label: "Price in USD",
-        data: [200, 700],
-        backgroundColor: ["#ffbb11", "#ecf0f1"],
-      },
-    ],
-  };
+const Chart = ({ chartData, title = "Chart title", horizontal = false }) => {
+  // const cgf = {
+  //   labels: ["first", "second"],
+  //   datasets: [
+  //     {
+  //       label: "Price in USD",
+  //       data: [200, 700],
+  //       backgroundColor: ["#ffbb11", "#ecf0f1"],
+  //     },
+  //   ],
+  // };
 
   console.log(chartData);
 
   return (
     <div>
       <Bar
-        data={cgf}
+        data={chartData}
         options={{
           plugins: {
             title: {
               display: true,
-              text: "Cryptocurrency prices",
+              text: title,
+              font: {
+                size: 17,
+              },
             },
             legend: {
               display: true,
               position: "bottom",
             },
           },
+          indexAxis: horizontal && "y",
         }}
       />
     </div>
