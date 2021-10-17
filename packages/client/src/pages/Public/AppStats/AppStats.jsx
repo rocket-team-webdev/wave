27d980c4e77ea694df829a85d78ba1d4dde34c4e
Brahim <brahimcasas@hotmail.es>
2021-10-17 13:02:52 +0200

    feat: app stats finished

--- packages/client/src/pages/Public/AppStats/AppStats.jsx
+++ packages/client/src/pages/Public/AppStats/AppStats.jsx
@@ -6,35 +6,52 @@ import AccountSideBar from "../../../components/AccountSideBar";
 import FormWrapper from "../../../components/FormWrapper";
 import Chart from "../../../components/Chart";
 import { getAllTracks } from "../../../api/tracks-api";
+import { getAllGenres } from "../../../api/genre-api";
 
 export default function AppStats() {
   const [loadStatus, setLoadStatus] = useState(false);
   const [topSongs, setTopSongs] = useState([]);
+  const [topGenres, setTopGenres] = useState([]);
+
+  const config = {
+    backgroundColor: [
+      "rgba(255, 99, 132, 0.2)",
+      "rgba(255, 159, 64, 0.2)",
+      "rgba(255, 205, 86, 0.2)",
+      "rgba(75, 192, 192, 0.2)",
+      "rgba(54, 162, 235, 0.2)",
+    ],
+    borderColor: [
+      "rgb(255, 99, 132)",
+      "rgb(255, 159, 64)",
+      "rgb(255, 205, 86)",
+      "rgb(75, 192, 192)",
+      "rgb(54, 162, 235)",
+    ],
+    borderWidth: 1,
+  };
 
   useEffect(async () => {
     const { data } = await getAllTracks();
-
     setTopSongs({
       labels: data.tracks.map((song) => song.name),
       datasets: [
         {
           label: "Popularity",
           data: data.tracks.map((song) => song.popularity),
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-            "rgba(255, 205, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-          ],
-          borderColor: [
-            "rgb(255, 99, 132)",
-            "rgb(255, 159, 64)",
-            "rgb(255, 205, 86)",
-            "rgb(75, 192, 192)",
-            "rgb(54, 162, 235)",
-          ],
-          borderWidth: 1,
+          ...config,
+        },
+      ],
+    });
+
+    const { data: genreData } = await getAllGenres(0, 5);
+    setTopGenres({
+      labels: genreData.genres.map((genre) => genre.name),
+      datasets: [
+        {
+          label: "Popularity",
+          data: genreData.genres.map((genre) => genre.popularity),
+          ...config,
         },
       ],
     });
@@ -65,7 +82,7 @@ export default function AppStats() {
                   // horizontal
                 />
                 <Chart
-                  chartData={topSongs}
+                  chartData={topGenres}
                   title="Top reproduced genres"
                   type="pie"
                 />
