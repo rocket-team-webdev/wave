7e103c24e62033b41ac54a8399f4fed144a3de52
Brahim <brahimcasas@hotmail.es>
2021-10-17 12:49:13 +0200

    feat: top genres backend

--- packages/server/src/controllers/genre-controller.js
+++ packages/server/src/controllers/genre-controller.js
@@ -17,6 +17,37 @@ async function getGenres(req, res, next) {
   }
 }
 
+async function getPopularGenres(req, res, next) {
+  try {
+    const { page = 0, limit = 10 } = req.query;
+
+    const genres = await db.Track.aggregate([
+      { $group: { _id: "$genreId", popularity: { $sum: "$popularity" } } },
+      {
+        $lookup: {
+          from: "genres",
+          localField: "_id",
+          foreignField: "_id",
+          as: "genres",
+        },
+      },
+      { $unwind: { path: "$genres" } },
+      { $project: { _id: 1, popularity: 1, name: "$genres.name" } },
+    ])
+      .sort({ popularity: -1 })
+      .skip(parseInt(page) * parseInt(limit))
+      .limit(parseInt(limit));
+
+    res.status(200).send({ genres });
+  } catch (err) {
+    res.status(404).send({
+      error: err.message,
+    });
+    next(err);
+  }
+}
+
 module.exports = {
   getGenres,
+  getPopularGenres,
 };
