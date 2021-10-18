9ca59f39f617f32620fcdea1974acd7a6faac93d
Ricard Garcia <imricardgarcia@gmail.com>
2021-10-18 11:55:03 +0200

    feat: added responsive font-size to jumbos and subtitles

--- packages/client/src/pages/Public/Home/Home.jsx
+++ packages/client/src/pages/Public/Home/Home.jsx
@@ -46,14 +46,14 @@ export default function Home() {
 
   return (
     <Layout isNegative>
-      <div className="d-flex justify-content-between align-items-start row p-0 g-4 pb-4 pb-sm-0">
+      <div className="d-flex justify-content-between align-items-start row p-0 g-4 pt-4 pt-md-0 pb-4 pb-sm-0">
         {/* Top part */}
-        <div className="d-flex top-part row p-0 m-0 mb-5">
+        <div className="d-flex top-part row p-0 m-0 mb-3 mb-md-5">
           {loading ? (
             <Spinner isNegative />
           ) : (
             <motion.h1
-              className="fnt-light fnt-page-title text-break col col-12 col-md-8 pt-3 truncate"
+              className="fnt-light fnt-page-title text-break col col-12 col-md-8 px-3 px-sm-0 pt-3"
               variants={fromBottom}
               initial="hidden"
               animate="visible"
