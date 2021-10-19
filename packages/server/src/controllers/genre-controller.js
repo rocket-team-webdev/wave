7a61c2345b99e4f5479323b09544741a46bc5ac3
Mathias Fortuna <66578026+mhfortuna@users.noreply.github.com>
2021-10-19 12:30:51 +0200

    Tech/sprint 5/303/fill genres popularity (#304)
    * refactor: packages json changed to dev
    
    * feat: created updateGenresPopularity in node
    
    * fix: console error when track not found
    
    * feat: updated getPopularGenres

--- packages/server/src/controllers/genre-controller.js
+++ packages/server/src/controllers/genre-controller.js
@@ -21,19 +21,7 @@ async function getPopularGenres(req, res, next) {
   try {
     const { page = 0, limit = 10 } = req.query;
 
-    const genres = await db.Track.aggregate([
-      { $group: { _id: "$genreId", popularity: { $sum: "$popularity" } } },
-      {
-        $lookup: {
-          from: "genres",
-          localField: "_id",
-          foreignField: "_id",
-          as: "genres",
-        },
-      },
-      { $unwind: { path: "$genres" } },
-      { $project: { _id: 1, popularity: 1, name: "$genres.name" } },
-    ])
+    const genres = await db.Genre.find({}, { _id: 1, popularity: 1, name: 1 })
       .sort({ popularity: -1 })
       .skip(parseInt(page) * parseInt(limit))
       .limit(parseInt(limit));
@@ -46,6 +34,37 @@ async function getPopularGenres(req, res, next) {
     next(error);
   }
 }
+
+// ! old functionality
+// async function getPopularGenres(req, res, next) {
+//   try {
+//     const { page = 0, limit = 10 } = req.query;
+
+//     const genres = await db.Track.aggregate([
+//       { $group: { _id: "$genreId", popularity: { $sum: "$popularity" } } },
+//       {
+//         $lookup: {
+//           from: "genres",
+//           localField: "_id",
+//           foreignField: "_id",
+//           as: "genres",
+//         },
+//       },
+//       { $unwind: { path: "$genres" } },
+//       { $project: { _id: 1, popularity: 1, name: "$genres.name" } },
+//     ])
+//       .sort({ popularity: -1 })
+//       .skip(parseInt(page) * parseInt(limit))
+//       .limit(parseInt(limit));
+
+//     res.status(200).send({ genres });
+//   } catch (error) {
+//     res.status(500).send({
+//       error: error.message,
+//     });
+//     next(error);
+//   }
+// }
 
 module.exports = {
   getGenres,
