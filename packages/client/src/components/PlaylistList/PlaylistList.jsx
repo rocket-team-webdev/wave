293c16b86a6f03dff16829898a3dfc62e78044b3
Nachomontoya <montoyanacho@gmail.com>
2021-10-19 09:11:31 +0200

    feat: added thumbnail on blur to playlist card

--- packages/client/src/components/PlaylistList/PlaylistList.jsx
+++ packages/client/src/components/PlaylistList/PlaylistList.jsx
@@ -32,6 +32,7 @@ function PlaylistList({
           playlistName={playlist.name}
           userId={playlist.userId}
           isFollowed={playlist.isFollowed}
+          thumbnail={playlist.thumbnail}
           colsMd={colsMd}
           updateFollowedView={handleAddFollowedColumn}
         />
