0dfe150e5a44f30413a303e1ff8081044ba02ff4
Nachomontoya <montoyanacho@gmail.com>
2021-10-20 10:53:47 +0200

    feat: finished email verification page

--- packages/client/src/pages/Public/VerifyEmail/VerifyEmail.jsx
+++ packages/client/src/pages/Public/VerifyEmail/VerifyEmail.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-nested-ternary */
 import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Layout from "../../../components/Layout";
 import FormWrapper from "../../../components/FormWrapper";
 import Button from "../../../components/Button";
@@ -8,25 +8,19 @@ import Button from "../../../components/Button";
 import { PUBLIC } from "../../../constants/routes";
 
 function Reauthenticate() {
-  const history = useHistory();
-
-  console.log(PUBLIC.HOME);
-  console.log(history);
-
   return (
     <Layout>
       <div className="row">
         <div className="col col-6 m-auto">
           <FormWrapper formTitle="Email verification">
-            <p className="mt-5 fnt-song-bold text-danger">
-              Your email account has been successfully verified. Now you can log
-              in. Click on the button and you will be redirected to the Log-in
-              page.
+            <p className="mt-2 fnt-song-bold fnt-secondary">
+              Your email account has been successfully verified! Now you can log
+              in. Click on the button below to be redirected to the Log-in page.
             </p>
             <div className="row">
               <div className="mt-5 col-auto ms-auto">
-                <Link to={PUBLIC.HOME}>
-                  <Button>Go Home</Button>
+                <Link to={PUBLIC.SIGN_IN}>
+                  <Button>Log in</Button>
                 </Link>
               </div>
             </div>
